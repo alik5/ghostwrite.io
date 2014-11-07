d37758032605f013a559fab34351fc158ed26fe8
Ali FitzGerald <alikfitz@gmail.com>
2014-11-06 19:15:42 -0500

    added remodal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,27 @@ $(document).ready(function(){
       console.log('hi');
       
 });
+
+$(document).on('open', '.remodal', function () {
+    console.log('open');
+});
+
+$(document).on('opened', '.remodal', function () {
+    console.log('opened');
+});
+
+$(document).on('close', '.remodal', function () {
+    console.log('close');
+});
+
+$(document).on('closed', '.remodal', function () {
+    console.log('closed');
+});
+
+$(document).on('confirm', '.remodal', function () {
+    console.log('confirm');
+});
+
+$(document).on('cancel', '.remodal', function () {
+    console.log('cancel');
+});
