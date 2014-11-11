da58c236de55d7c1131d8be9a10db347632e0632
Ali FitzGerald <alikfitz@gmail.com>
2014-11-11 14:54:22 -0500

    updated formatting on static_pages

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,69 @@
+
+
+
+
+$('a.pop_personal').on('click',function(e) {
+$('#index').empty();
+console.log("emptied")
+//$('#index').hide();
+$('#index').load('personal.html')
+console.log("loaded")
+});
+
+$('a.pop_network').on('click',function(e) {
+$('#index').empty();
+console.log("emptied") 
+$('#index').load('network.html');
+console.log("loaded")
+});
+
+$('a.pop_coverLetter').on('click',function(e) {
+$('#index').empty();
+console.log("emptied") 
+$('#index').load('cover_letter.html');
+console.log("loaded")
+});
+
+$('a.pop_thankYou').on('click',function(e) {
+$('#index').empty();
+console.log("emptied") 
+$('#index').load('thank_you.html');
+console.log("loaded")
+});
+
+$('a.pop_freelancing').on('click',function(e) {
+$('#index').empty();
+console.log("emptied") 
+$('#index').load('freelancing.html');
+console.log("loaded")
+});
+
+$('a.pop_dating').on('click',function(e) {
+$('#index').empty();
+console.log("emptied") 
+$('#index').load('dating.html');
+console.log("loaded")
+});
+
+$('a.pop_distance').on('click',function(e) {
+$('#index').empty();
+console.log("emptied") 
+$('#index').load('distance.html');
+console.log("loaded")
+});
+
+$('a.pop_thanks').on('click',function(e) {
+$('#index').empty();
+console.log("emptied") 
+$('#index').load('thanks.html');
+console.log("loaded")
+});
+
+$('a.pop_ask').on('click',function(e) {
+$('#index').empty();
+console.log("emptied") 
+$('#index').load('ask.html');
+console.log("loaded")
+});
+
+console.log('I read the whole thing')
