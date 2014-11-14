ff7e0001ab0a4fe69ff3aeb4ff80f83943a9d99b
Ali FitzGerald <alikfitz@gmail.com>
2014-11-13 20:36:25 -0500

    add files to assets config initializers

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( search.js bootstrap.js static_pages.js  )
+Rails.application.config.assets.precompile += %w( search.js bootstrap.js static_pages.js button.css.scss static_pages.css.scss )
