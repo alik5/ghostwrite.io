c95ec6405aff06f2d5316c12aaf7611cd1bd56bb
Ali FitzGerald <alikfitz@gmail.com>
2014-11-22 15:42:53 -0500

    migrated questions table

---
+++ db/migrate/20141122204156_create_questions_table.rb
@@ -0,0 +1,6 @@
+class CreateQuestionsTable < ActiveRecord::Migration
+  def change
+    create_table :questions_tables do |t|
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122204002) do
+ActiveRecord::Schema.define(version: 20141122204156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(version: 20141122204002) do
   end
 
   add_index "letters", ["properties"], name: "letters_properties", using: :gin
+
+  create_table "questions_tables", force: true do |t|
+  end
 
   create_table "surveys", force: true do |t|
     t.datetime "created_at"
