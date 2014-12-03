3f20c0bdfa1d12cfac68886389f1a3e505438808
Ali FitzGerald <alikfitz@gmail.com>
2014-12-02 19:28:24 -0500

    before adding column to letter_fields

--- app/models/letter_field.rb
+++ app/models/letter_field.rb
@@ -2,12 +2,8 @@ class LetterField < ActiveRecord::Base
   belongs_to :letter_type
   has_many :answers
 
-def increment(attribute, by = 1)
-  self[attribute] ||= 0
-  self[attribute] += by
-  self
-end
-	
+
+
 
   
 end
