973a5d55b2acb0284c5d9a6be796d041fa7b9f1c
Ali FitzGerald <alikfitz@gmail.com>
2014-12-04 11:01:18 -0500

    added js font tags

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,6 +50,20 @@ $(document).ready(function(){
   });
 });
 
+<script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'Raleway+Dots::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); </script>
+
 
 
 
