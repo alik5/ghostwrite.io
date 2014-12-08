c7b7edc6070c35f54f5f52d263dfb14c4096055b
Ali FitzGerald <alikfitz@gmail.com>
2014-12-08 08:59:10 -0500

    added columns to contact table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207202631) do
+ActiveRecord::Schema.define(version: 20141208135725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 20141207202631) do
   create_table "contacts", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "email"
+    t.text     "message"
   end
 
   create_table "ghostwrites", force: true do |t|
