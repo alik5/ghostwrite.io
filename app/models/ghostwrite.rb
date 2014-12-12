ca2ce812b136abcaab39956975934e126ecfd3fb
Ali FitzGerald <alikfitz@gmail.com>
2014-12-11 20:40:33 -0500

    added to ghostwrite model

--- app/models/ghostwrite.rb
+++ app/models/ghostwrite.rb
@@ -4,6 +4,7 @@ class Ghostwrite < ActiveRecord::Base
 
 	has_attached_file :photo, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+    validates_attachment_content_type :photo, content_type: [ "image/jpg", "image/jpeg", "image/png", "image/gif", "application/pdf" ]
 
 end
 
