76a66fc21f04f8138d50fd7b9f0ab1b0bf769c7e
Ali FitzGerald <alikfitz@gmail.com>
2014-12-13 17:56:21 -0500

    added params to images

--- app/models/ghostwrite.rb
+++ app/models/ghostwrite.rb
@@ -9,6 +9,10 @@ class Ghostwrite < ActiveRecord::Base
 	has_attached_file :photo,
 	:storage => :s3,
   	:bucket => ENV['S3_BUCKET_NAME']
+  	styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'}
 	
 
 	validates_attachment_content_type :photo, content_type: [ "image/jpg", "image/jpeg", "image/png", "image/gif", "application/pdf" ]
