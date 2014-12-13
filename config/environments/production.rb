e49388f9403176e63d5b6ad98040e6ffb96951e4
Ali FitzGerald <alikfitz@gmail.com>
2014-12-13 16:42:00 -0500

    trial

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,10 +21,12 @@ config.action_mailer.smtp_settings = {
     :domain => 'www.ghostwrite.io', # your domain to identify your server when connecting
   }
 
-  config.paperclip_defaults = {
+config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => 'ghostwrite.io'
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 
