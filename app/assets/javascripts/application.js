8b4b6f1a7a709f4301d3793b5a38f883b446d102
Ali FitzGerald <alikfitz@gmail.com>
2015-01-21 19:26:21 -0500

    new js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@
 //= require nprogress
 //= require nprogress-turbolinks
 //= require nprogress-ajax
+//= require letters
 //= require wow
 //= require bootstrap-wysihtml5
 //= require bootstrap-wysihtml5/locales
@@ -93,57 +94,7 @@ $('span.next, span.prev').click(function(e){
 
   $('#some-textarea').wysihtml5();
 
-  //cover letter//
 
-
-  $('.next').on('click', function(){
-  $('#originHint').hide();
-  });
-
-
-  $('input#one_word').on('focus', function(){
-  $("#oneWordHint").show();
-  });
-
-  $('input#one_word').on('blur', function(){
-  $("#oneWordHint").hide();
-  });
-
-  $('input#last_job').on('focus', function(){
-  $("#lastJobHint").show();
-  });
-
-  $('input#last_job').on('blur', function(){
-  $("#lastJobHint").hide();
-  });
-
-  $('input#position').on('focus', function(){
-  $("#positionHint").show();
-  });
-
-  $('input#position').on('blur', function(){
-  $("#positionHint").hide();
-  });
-
-  $('input#interest').on('focus', function(){
-  $("#interestHint").show();
-  });
-  
-  $('input#interest').on('blur', function(){
-  $("#interestHint").hide();
-  });
-
-  $('input#skill1').on('focus', function(){
-  $("#skillHint").show();
-  });
-
-  $('input#skill2').on('blur', function(){
-  $("#skillHint").hide();
-  });
-
-  $('#company').on('keyup', function(){
-  $('#inputDiv').html($('#company').val()).append(" would be lucky to have you!");
-  });
    
 
 
--- app/assets/javascripts/letters.js
+++ app/assets/javascripts/letters.js
@@ -1,2 +1,53 @@
+  //cover letter//
+$(document).ready(function(){
 
+  $('.next').on('click', function(){
+  $('#originHint').hide();
+  });
+
+
+  $('input#one_word').on('focus', function(){
+  $("#oneWordHint").show();
+  });
+
+  $('input#one_word').on('blur', function(){
+  $("#oneWordHint").hide();
+  });
+
+  $('input#last_job').on('focus', function(){
+  $("#lastJobHint").show();
+  });
+
+  $('input#last_job').on('blur', function(){
+  $("#lastJobHint").hide();
+  });
+
+  $('input#position').on('focus', function(){
+  $("#positionHint").show();
+  });
+
+  $('input#position').on('blur', function(){
+  $("#positionHint").hide();
+  });
+
+  $('input#interest').on('focus', function(){
+  $("#interestHint").show();
+  });
+  
+  $('input#interest').on('blur', function(){
+  $("#interestHint").hide();
+  });
+
+  $('input#skill1').on('focus', function(){
+  $("#skillHint").show();
+  });
+
+  $('input#skill2').on('blur', function(){
+  $("#skillHint").hide();
+  });
+
+  $('#company').on('keyup', function(){
+  $('#inputDiv').html($('#company').val()).append(" would be lucky to have you!");
+  });
+ });
 
