71e081a87f65ce8828eab1e1b62fa51eb95b3c22
Ali FitzGerald <alikfitz@gmail.com>
2015-01-26 17:19:17 -0500

    big error redis

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.gem 'redis'
+
 
 if ENV["REDISCLOUD_URL"]
     $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
