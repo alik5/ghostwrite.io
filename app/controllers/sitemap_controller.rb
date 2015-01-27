c755d39eab4cb790e84536e233e982bceb426ea2
Ali FitzGerald <alikfitz@gmail.com>
2015-01-27 10:11:20 -0500

    fix sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -3,11 +3,7 @@ class SitemapController < ApplicationController
 
   def index
     headers['Content-Type'] = 'application/xml'
-    last_letter_type = LetterType.last
-    if stale?(:etag => last_post, :last_modified => last_post.updated_at.utc)
-      respond_to do |format|
-        format.xml { @letter_types = Letter.sitemap } # sitemap is a named scope
-      end
-    end
+    
+     
   end
 end
