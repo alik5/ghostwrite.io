ed259a51c2073b388b3d7ec469c7a604a9a8eaca
Ali FitzGerald <alikfitz@gmail.com>
2015-01-27 10:06:53 -0500

    added lines to sitemap controller

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -3,10 +3,10 @@ class SitemapController < ApplicationController
 
   def index
     headers['Content-Type'] = 'application/xml'
-    last_post = Post.last
+    last_letter_type = LetterType.last
     if stale?(:etag => last_post, :last_modified => last_post.updated_at.utc)
       respond_to do |format|
-        format.xml { @posts = Post.sitemap } # sitemap is a named scope
+        format.xml { @letter_types = Letter.sitemap } # sitemap is a named scope
       end
     end
   end
