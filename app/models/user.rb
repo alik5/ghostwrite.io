09615bde22e3e0a2144f91155e2d64db738045e1
Ali FitzGerald <alikfitz@gmail.com>
2015-02-02 20:06:33 -0500

    broke it

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,6 @@ class User < ActiveRecord::Base
   	"#{self.first_name} #{self.last_name}"
 	end
 
-	def self.to_mandrill_to(users)
- 	users.map{|user| {:email => user.email}}
-	end
-
-	def self.to_mandrill_merge_vars(users)
-
- 	users.map{|user| {:rcpt => user.email, :vars => [{:name => 'first_name', :content => user.first_name}]}}
-
-	end
 
  
 
