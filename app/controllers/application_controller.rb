6cbc47ab36eb19b5f54ba47eb58d49f479035103
Ali FitzGerald <alikfitz@gmail.com>
2015-02-27 15:21:29 -0500

    changed bootstrap mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   #before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
  def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :profile_name, :email, :password, :password_confirmation) }
   end
